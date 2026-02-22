20532c1546f5d8904e162526d4c5345b3cf2b6b6
DK <dokter322@yandex.ru>
2026-02-23 02:21:19 +0300

    init tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,23 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { routeTree } from "./routeTree.gen";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+const router = createRouter({
+  routeTree,
+  defaultPreload: "intent",
+  scrollRestoration: true,
+});
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
